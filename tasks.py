26de29b2c93bc5e670a77403b3cf2e4ab615edef
Mohamad Juma <pythonentwickler76@gmail.com>
2022-07-31 16:02:59 +0200

    add tasks file

---
+++ tasks.py
@@ -0,0 +1,45 @@
+#1
+x=false
+
+#2
+y=10
+
+#3
+z=2.5
+
+#4
+s='mohamad'
+
+#5
+float(y)
+10.0
+
+#6
+no we can't
+ValueError
+
+#7
+l=[1,2,3,4,5]
+
+#8
+(10,11,12,13,14,15)
+
+#9
+tuple(l)
+(1, 2, 3, 4, 5)
+
+#10
+r={'mohamad':60 ,'ali':70 ,'reem':80}
+
+#11
+yes we can but we don't need to use it because python underdtand that
+we finsh the line without ;
+
+#12
+python is interpreted
+
+#13
+In general, the difference is at a low level, for beginners and beginners,
+and it may be age-related and limited.
+The high level is higher than the low, with higher quality
+and efficiency, and a developed level, which is a sequelto the low level.
